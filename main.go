a3c1c0ac62cb0d7bb353bf55b5b5096883163023
David Levy <lennox.davidlevy@gmail.com>
2021-01-27 16:03:19 -0500

    first commit

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
-
+	//david := person{firstName: "David", lastName: "Levy"}
+	var david person
+	fmt.Println(david)
 }
